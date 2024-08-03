6e91754628f1ded4228a344f053158239fb5cd30
AbelHaro <ahararm@upv.edu.es>
2024-08-03 20:14:39 +0200

    chore: Update CORS middleware to allow any localhost port and vercel.app subdomains

--- backend/api/middleware/cors.js
+++ backend/api/middleware/cors.js
@@ -1,19 +1,23 @@
-import cors from 'cors'
+import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
-    /^http:\/\/localhost:\d+$/
-]
+  /^http:\/\/localhost:\d+$/, // Allow any localhost port
+  /\.vercel\.app$/, // Allow any subdomain of vercel.app
+];
 
-export const corsMiddleware = () => cors({
+export const corsMiddleware = () =>
+  cors({
     origin: (origin, callback) => {
-        if (!origin) return callback(null, true)
-        
-        const isAccepted = ACCEPTED_ORIGINS.some((pattern) => pattern.test(origin));
-        
-        if (isAccepted) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-})
+      if (!origin) return callback(null, true);
+
+      const isAccepted = ACCEPTED_ORIGINS.some((pattern) =>
+        pattern.test(origin)
+      );
+
+      if (isAccepted) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  });
